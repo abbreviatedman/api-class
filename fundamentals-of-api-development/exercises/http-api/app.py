4360eddbd4b131a320e177f78c5809d047cecc16
Colin Jaffe <balloonasaurus@gmail.com>
2023-01-30 08:00:34 -0500

    Updates.

--- fundamentals-of-api-development/exercises/http-api/app.py
+++ fundamentals-of-api-development/exercises/http-api/app.py
@@ -31,26 +31,3 @@ def post_data_to_json(filename, data):
     """
     with open(filename, "w") as file:
         json.dump(data, file, indent=2)
-
-@app.get("/characters")
-def get_characters():
-    return get_data_from_json("characters.json")
-
-@app.get("/characters/<string:id>")
-def get_character(id):
-    characters = get_data_from_json("characters.json")
-    for index, character in enumerate(characters):
-        if character["id"] == int(id):
-            return character
-
-    return 404
-
-@app.post("/characters")
-def add_character():
-    request_data = request.get_json()
-    characters = get_data_from_json("characters.json")
-    characters.append(request_data)
-    post_data_to_json("characters.json", characters)
-
-    return request_data, 201
-
